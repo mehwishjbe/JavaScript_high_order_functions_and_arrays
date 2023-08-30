38c1ad60b8f6456d2c2e1ab4abebc915bb46e021
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-30 20:55:42 +0200

    filter_arrowFunctions

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const companies=[
     {name:"Company One", category:"Finance", start:1981, end:2003},
     {name:"Company Two", category:"Retail", start:1992, end:2008},
-    {name:"Company Three", category:"Auto", start:1999, end:2007},
-    {name:"Company Four", category:"Retail", start:1989, end:2010},
+    {name:"Company Three", category:"Auto", start:1999, end:2003},
+    {name:"Company Four", category:"Retail", start:1989, end:2003},
     {name:"Company Five", category:"Technology", start:2009, end:2014},
     {name:"Company Six", category:"Finance", start:1987, end:2010},
     {name:"Company Seven", category:"Auto", start:1986, end:1996},
     {name:"Company Eight", category:"Technology", start:2011, end:2016},
-    {name:"Company Nine", category:"Retail", start:1981, end:1989},
+    {name:"Company Nine", category:"Retail", start:1981, end:2003},
 ];
 
 const ages = [33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
@@ -38,4 +38,35 @@ const canDrink1 = ages.filter(function(age){
     }
 });
 console.log(canDrink1);
+/* 
+let retail = [];
+for(let i = 0; i<companies.length; i++){
+    if (companies[i].) {
+        retail.push(companies[i]);
+        
+    }
+}
 
+console.log(retail); */
+
+
+const retail = companies.filter(function(companies){
+
+    if(companies.category === 'Retail'){
+        return true;
+    }
+});
+console.log(retail);
+
+
+
+/* let endYear = companies.filter(function(companies){
+
+    if(companies.end === 2003){
+        return true;
+    }
+});
+console.log(endYear); */
+
+const endYear1 = companies.filter(companies => companies.end === 2003 );
+console.log(endYear1);
