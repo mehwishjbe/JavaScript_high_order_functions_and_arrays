9919ebaba83fc8454aa87d35174f6725281cdddb
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-30 20:33:05 +0200

    forEach

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ const companies=[
     {name:"Company Nine", category:"Retail", start:1981, end:1989},
 ];
 
+const ages = [33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
+
+/* for(let i=0; i<companies.length; i++){
+    console.log(companies[i]);
+} */
+
+//FOR EACH
+companies.forEach(function(companies){
+    console.log(companies.name, companies.end);
+    console.log(companies.start);
+});
