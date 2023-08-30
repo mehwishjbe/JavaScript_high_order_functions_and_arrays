3566f06b15918f05d40f03b9a62869eec97c3ab9
mehwishjbe <mehwish.jbe@gmail.com>
2023-08-30 20:44:32 +0200

    filter

--- main.js
+++ main.js
@@ -16,8 +16,26 @@ const ages = [33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
     console.log(companies[i]);
 } */
 
-//FOR EACH
+/* //FOR EACH
 companies.forEach(function(companies){
     console.log(companies.name, companies.end);
     console.log(companies.start);
+}); */
+
+
+//FILTER
+let canDrink = [];
+for(let i=0; i<ages.length; i++){
+    if(ages[i] <= 21){
+        canDrink.push(ages[i]);
+    }
+}
+console.log(canDrink);
+
+const canDrink1 = ages.filter(function(age){
+    if (age >= 21) {
+        return true;
+    }
 });
+console.log(canDrink1);
+
